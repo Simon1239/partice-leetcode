04c13836bd1fb073e8d1eb6c1a42f56f2a54c51c
Simon1239 <106095678+Simon1239@users.noreply.github.com>
2023-12-09 22:24:03 +0800

    fix:调整文件结构

---
+++ 二叉树/Day6-验证二叉搜索树.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self) -> str:
+        return str(self.val)
+    
