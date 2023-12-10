80ff74470f215305e3d312b0a70c724dbeb956fc
Simon1239 <106095678+Simon1239@users.noreply.github.com>
2023-12-10 23:40:45 +0800

    feat:优化删除元素

--- 双指针/Day7-移除元素.py
+++ 双指针/Day7-移除元素.py
@@ -12,10 +12,23 @@ class Solution:
                 left += 1
         return left, nums[:left]
     
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        left, right = 0, len(nums)
+        while left < right:
+            if nums[left] == val:
+                nums[left] = nums[right-1]
+                right -= 1
+            else:
+                left += 1
+        return left, nums[:left]
+    
 if __name__ == '__main__':
     # nums = [3, 2, 2, 3]
-    nums = [0,1,2,2,3,0,4,2]
+    # nums = [0,1,2,2,3,0,4,2]
+    nums = [1,2,3,4,5]
     # val = 3
-    val = 2
+    # val = 2
+    val = 1
     S =Solution()
     print(S.removeElement(nums, val))
